44d754155c3cc0a45f2b145cfb7073083021bd0d
Jon Jandoc <jon.jandoc@gmail.com>
2020-05-29 12:02:14 -0700

    Update package settings

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  css: {
+    extract: false,
+  },
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule.use('vue-svg-loader').loader('vue-svg-loader');
+  },
+};
