015f5a9c4d98c41490ea0be69c79191cfd9c0e81
Jon Jandoc <jon.jandoc@gmail.com>
2020-07-31 14:38:54 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Alert from '@/constants/Alert';
 import Button from '@/constants/Button';
 import Form from '@/constants/Form';
 import InputMasks from '@/constants/InputMasks';
+import Notification from '@/constants/Notification';
 import Typography from '@/constants/Typography';
 
 export { default as AlertBar } from '@/components/AlertBar/AlertBar.vue';
@@ -25,6 +26,8 @@ export { default as SwitchGroup } from '@/components/SwitchGroup/SwitchGroup.vue
 export { default as SwitchInput } from '@/components/SwitchInput/SwitchInput.vue';
 export { default as TextInput } from '@/components/TextInput/TextInput.vue';
 export { default as TextareaInput } from '@/components/TextareaInput/TextareaInput.vue';
+export { default as ToastNotification } from '@/components/ToastNotification/ToastNotification.vue';
+export { default as ToastNotifications } from '@/components/ToastNotifications/ToastNotifications.vue';
 export { default as TwoColumnMultiSelect } from '@/components/TwoColumnMultiSelect/TwoColumnMultiSelect.vue';
 export { default as TypeBody } from '@/components/TypeBody/TypeBody.vue';
 export { default as TypeDisplay } from '@/components/TypeDisplay/TypeDisplay.vue';
@@ -32,4 +35,11 @@ export { default as TypeHeading } from '@/components/TypeHeading/TypeHeading.vue
 export { default as TypeOverline } from '@/components/TypeOverline/TypeOverline.vue';
 export { default as UserAvatar } from '@/components/UserAvatar/UserAvatar.vue';
 
-export const constants = { Alert, Button, Form, InputMasks, Typography };
+export const constants = {
+  Alert,
+  Button,
+  Form,
+  InputMasks,
+  Notification,
+  Typography,
+};
