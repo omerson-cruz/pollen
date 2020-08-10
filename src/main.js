a957b52ed5fa8516b63d83afaa06b30bad542776
Jon Jandoc <jon.jandoc@gmail.com>
2020-08-10 15:57:26 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -17,6 +17,7 @@ export { default as BaseTag } from '@/components/BaseTag/BaseTag.vue';
 export { default as CheckboxGroup } from '@/components/CheckboxGroup/CheckboxGroup.vue';
 export { default as CheckboxInput } from '@/components/CheckboxInput/CheckboxInput.vue';
 export { default as DateInput } from '@/components/DateInput/DateInput.vue';
+export { default as FancyIcon } from '@/components/FancyIcon/FancyIcon.vue';
 export { default as IconButton } from '@/components/IconButton/IconButton.vue';
 export { default as PasswordInput } from '@/components/PasswordInput/PasswordInput.vue';
 export { default as RadioGroup } from '@/components/RadioGroup/RadioGroup.vue';
@@ -25,6 +26,7 @@ export { default as RadioToggle } from '@/components/RadioToggle/RadioToggle.vue
 export { default as SelectInput } from '@/components/SelectInput/SelectInput.vue';
 export { default as SwitchGroup } from '@/components/SwitchGroup/SwitchGroup.vue';
 export { default as SwitchInput } from '@/components/SwitchInput/SwitchInput.vue';
+export { default as TaskCard } from '@/components/TaskCard/TaskCard.vue';
 export { default as TextInput } from '@/components/TextInput/TextInput.vue';
 export { default as TextareaInput } from '@/components/TextareaInput/TextareaInput.vue';
 export { default as ToastNotification } from '@/components/ToastNotification/ToastNotification.vue';
